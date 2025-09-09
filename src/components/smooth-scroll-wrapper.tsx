691c1a82a3b62ad7d168cc0fa37652e0e2e0a7f0
ziyad <ziadr265@hotmail.com>
2025-09-09 22:21:32 +0100

    feat: setup lenis for smooth scrolling animatio

---
+++ src/components/smooth-scroll-wrapper.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Lenis from "lenis";
+import { type PropsWithChildren, useEffect } from "react";
+
+export default function SmoothScrollWrapper({ children }: PropsWithChildren) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 2,
+    });
+
+    const onFrame = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(onFrame);
+    };
+    requestAnimationFrame(onFrame);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+  return children;
+}
